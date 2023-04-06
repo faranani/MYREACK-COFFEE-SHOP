4219f9dd1c6f8df2afa91c254784e58538f8dc17
faranani <39202758+faranani@users.noreply.github.com>
2023-04-06 19:38:34 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import Home from "./components/Home/home"
+import NavBar from "./components/NavBar/Navbar"
+
+import './App.css';
+// import fara from './image/fara/'
+
+function App() {
+  return (
+
+    <>
+      <NavBar />
+
+      <div>
+        <Home />
+      </div>
+
+
+    </>
+
+
+
+  );
+}
+
+export default App;
